7bf80e7d0710bf4512752d25a47449025449e2c3
kent espia <kentvirgil.espia@cit.edu>
2024-05-09 21:14:58 +0800

    kent main menu

--- src/main/java/com/example/casino/HelloApplication.java
+++ src/main/java/com/example/casino/HelloApplication.java
@@ -11,10 +11,10 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hi-lo.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Parent root = fxmlLoader.load();
         stage.setTitle("Hello!");
-        stage.setScene(new Scene(root)); // Removed setting scene here
+        stage.setScene(new Scene(root));
         stage.show();
     }
 
--- src/main/java/com/example/casino/HelloController.java
+++ src/main/java/com/example/casino/HelloController.java
@@ -1,7 +1,15 @@
 package com.example.casino;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
     @FXML
@@ -10,5 +18,15 @@ public class HelloController {
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @FXML
+    public void onHiloBTNClick(ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("hi-lo.fxml"));
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) ((Button) actionEvent.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
     }
 }
